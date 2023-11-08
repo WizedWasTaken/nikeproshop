feabf43fe85dd40fa25b9a15fc85fbeb3b5bede5
WizedWasTaken <jegerfattig1@gmail.com>
2023-11-08 15:00:26 +0100

    Footer updated

--- src/components/FooterBar.vue
+++ src/components/FooterBar.vue
@@ -1,5 +1,67 @@
 <template>
   <footer>
-    <h1>Footer</h1>
+    <h2>Nike Pro Shop Stores</h2>
+    <h3>København</h3>
+    <p>Rådhusstræde 6, st.</p>
+    <p>1466 København K</p>
+    <h3>Aarhus</h3>
+    <p>Søndergade 70</p>
+    <p>8000 Aarhus</p>
+    <h3>Odense</h3>
+    <p>Vestergade 23</p>
+    <p>5000 Odense</p>
+    <h2>Åbningstider:</h2>
+    <p>Mandag - fredag: 10:00 - 18:00</p>
+    <p>Lørdag: 10:00 - 13:00</p>
+    <br />
+    <p>Telefon: (+45) 33 91 18 18</p>
+    <p>E-mail: online@NikeProShop.dk</p>
+    <h3>Nike Pro Shop på de sociale medier:</h3>
+    <div class="icons">
+      <img src="../assets/FacebookIcon.png" alt="" />
+      <img src="../assets/InstagramIcon.png" alt="" />
+      <img src="../assets/TwitterIcon.png" alt="" />
+    </div>
+    <div class="links">
+      <router-link to="/contact">Kontakt</router-link>
+      <router-link to="/about">Om Nike Pro Shop</router-link>
+    </div>
   </footer>
 </template>
+
+<style scoped lang="scss">
+$background-color: #ffa500;
+
+footer {
+  background-color: $background-color;
+  display: flex;
+  flex-direction: column;
+  color: white;
+  padding: 5px 30px;
+
+  h3 {
+    margin: 10px 0px;
+  }
+
+  p {
+    font-size: 1rem;
+    margin: 3px 0px;
+  }
+
+  .icons {
+    display: flex;
+    justify-content: space-around;
+  }
+
+  .links {
+    display: flex;
+    justify-content: space-around;
+    margin-top: 10px;
+
+    a {
+      color: white;
+      text-decoration: none;
+    }
+  }
+}
+</style>
